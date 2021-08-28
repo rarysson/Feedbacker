5ffa4aa23ba5d0267043d3da42ac711bc15cd9f4
Rarysson Guilherme <r_guilherme12@hotmail.com>
2021-08-27 21:38:51 -0300

    Adicionado modal de criar conta

--- src/services/authService.js
+++ src/services/authService.js
@@ -18,4 +18,24 @@ export default (httpCliente) => ({
       errors,
     };
   },
+  register: async ({ name, email, password }) => {
+    const response = await httpCliente.post("/auth/register", {
+      name,
+      email,
+      password,
+    });
+    let errors = null;
+
+    if (!response.data) {
+      errors = {
+        status: response.request.status,
+        statusText: response.request.statusText,
+      };
+    }
+
+    return {
+      data: response.data,
+      errors,
+    };
+  },
 });
